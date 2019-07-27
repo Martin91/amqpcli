89ac7f11c6fe93ccf52a2970d67f0a6a8e3576f9
zeqin.hong <zeqin.hong@shopee.com>
2019-07-28 00:51:43 +0800

    feat: enable to manage exchange

--- lib/handlers/__init__.py
+++ lib/handlers/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from lib.handlers.queue_handlers import *
+from lib.handlers.exchange_handlers import *
 
 __all__ = ['QueueDeclareHandler', 'QueueDeleteHandler', 'QueueBindHandler',
-           'QueueUnbindHandler', 'QueuePurgeHandler',]
+           'QueueUnbindHandler', 'QueuePurgeHandler',
+           'ExchangeDeclareHandler', 'ExchangeDeleteHandler']
